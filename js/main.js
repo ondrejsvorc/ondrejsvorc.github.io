e9498c822c33162df7c7588158e128c30ba2b2fd
Ondřej Švorc <52735242+ondrejsvorc@users.noreply.github.com>
2021-04-09 17:13:05 +0200

    Reveal effect added

--- js/main.js
+++ js/main.js
@@ -17,4 +17,23 @@ const footerParagraph = document.querySelector("footer p");
 //     }
 // });
 
+window.addEventListener("scroll", function() 
+{
+    let revealElements = document.querySelectorAll('.reveal')
+
+    for (let i = 0; i < revealElements.length; i++)
+    {
+        let revealPoint = 100;
+
+        if (revealElements[i].getBoundingClientRect().top < window.innerHeight - revealPoint)
+        {
+            revealElements[i].classList.add('active');
+        }
+        else 
+        {
+            revealElements[i].classList.remove('active');
+        }
+    }
+});
+
 footerParagraph.innerHTML += " " + new Date().getFullYear();
