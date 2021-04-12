f116951bafa5b8021166e2bb622fcf6c8014ae07
Ondřej Švorc <52735242+ondrejsvorc@users.noreply.github.com>
2021-04-12 21:01:39 +0200

    Updated images

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 
 const footerParagraph = document.querySelector("footer p");
 
-// window.addEventListener("scroll", function() 
+// window.addEventListener("scroll", function()
 // {
 //     let scrollY = window.scrollY;
 
@@ -20,49 +20,44 @@ const footerParagraph = document.querySelector("footer p");
 document.querySelector("body").style.visibility = "hidden";
 document.querySelector(".loader-container").style.visibility = "visible";
 
-window.addEventListener('load', (event) => 
-{
-    document.querySelector(".loader-container").style.display = "none";
-    document.querySelector("body").style.visibility = "visible";
+window.addEventListener("load", (event) => {
+  document.querySelector(".loader-container").style.display = "none";
+  document.querySelector("body").style.visibility = "visible";
 });
 
-window.addEventListener("scroll", function() 
-{
-    let revealElements = document.querySelectorAll('.reveal')
+window.addEventListener("scroll", function () {
+  let revealElements = document.querySelectorAll(".reveal");
 
-    for (let i = 0; i < revealElements.length; i++)
-    {
-        let revealPoint = 100;
+  for (let i = 0; i < revealElements.length; i++) {
+    let revealPoint = 100;
 
-        if (revealElements[i].getBoundingClientRect().top < window.innerHeight - revealPoint)
-        {
-            revealElements[i].classList.add('active');
-        }
-        else 
-        {
-            revealElements[i].classList.remove('active');
-        }
+    if (
+      revealElements[i].getBoundingClientRect().top <
+      window.innerHeight - revealPoint
+    ) {
+      revealElements[i].classList.add("active");
+    } else {
+      revealElements[i].classList.remove("active");
     }
+  }
 });
 
-const githubText = document.querySelector('.github-text');
-const emailText = document.querySelector('.email-text');
+const githubText = document.querySelector(".github-text");
+const emailText = document.querySelector(".email-text");
 
-githubText.style.display = 'none';
-emailText.style.display = 'none';
+githubText.style.display = "none";
+emailText.style.display = "none";
 
-async function ShowGitHub()
-{
-    githubText.classList.add('visible');
-    await new Promise(resolve => setTimeout(resolve, 3000));
-    githubText.classList.remove('visible');
+async function ShowGitHub() {
+  githubText.classList.add("visible");
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  githubText.classList.remove("visible");
 }
 
-async function ShowEmail()
-{
-    emailText.classList.add('visible');
-    await new Promise(resolve => setTimeout(resolve, 3000));
-    emailText.classList.remove('visible');
+async function ShowEmail() {
+  emailText.classList.add("visible");
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  emailText.classList.remove("visible");
 }
 
 footerParagraph.innerHTML += " " + new Date().getFullYear();
