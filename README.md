a84cad871c4826dda3bc27b6433d2f6630047422
ondrejsvorc <ondrejsvorc@email.cz>
2024-06-15 21:17:23 +0200

    Add new personal portfolio
    It's actually an old website I made back in secondary school but I still find the design nice so I decided to reuse it.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Personal Portfolio
+
+This project showcases my work and skills in web development. The portfolio is entirely built using HTML, CSS, and JavaScript (no need to make things harder than necessary).
+
+You can view the portfolio [here](https://ondrejsvorc.github.io/).
---
+++ js/main.js
@@ -0,0 +1,46 @@
+let tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+let tooltipList = tooltipTriggerList.map((tooltipTriggerEl) => {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+const section = document.querySelector("#about-me")
+
+if (section !== null) {
+  const about = document.querySelector(".nav-link.about").addEventListener('click', (e) => {
+    e.preventDefault();
+    section.scrollIntoView();
+  });
+}
+
+const footerParagraph = document.querySelector("footer p");
+
+document.querySelector("main").style.visibility = "hidden";
+document.querySelector("#navBar").style.visibility = "hidden";
+document.querySelector(".loader-container").style.visibility = "visible";
+
+window.addEventListener("load", (event) => {
+  document.querySelector(".loader-container").style.display = "none";
+  document.querySelector("main").style.visibility = "visible";
+  document.querySelector("#navBar").style.visibility = "visible";
+});
+
+window.addEventListener("scroll", function () {
+  let revealElements = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < revealElements.length; i++) {
+    let revealPoint = 100;
+
+    if (
+      revealElements[i].getBoundingClientRect().top <
+      window.innerHeight - revealPoint
+    ) {
+      revealElements[i].classList.add("active");
+    } else {
+      revealElements[i].classList.remove("active");
+    }
+  }
+});
+
+footerParagraph.innerHTML += " " + new Date().getFullYear();
